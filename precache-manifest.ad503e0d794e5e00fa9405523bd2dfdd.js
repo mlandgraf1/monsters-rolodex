cab5d3b0e02f9a11ee971d8bb702028faee44eb2
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2020-05-26 13:01:13 -0500

    Updates

---
+++ precache-manifest.ad503e0d794e5e00fa9405523bd2dfdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089f22df873f2c455c6f8e27bb52cc76",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a81f3ee4d4144f8c6d5",
+    "url": "/monsters-rolodex/static/css/main.1f434102.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a81f3ee4d4144f8c6d5",
+    "url": "/monsters-rolodex/static/js/main.997273a9.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
